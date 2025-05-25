9640a7e60ebf65c67fb50361eaca6a47b421130c
Allan Roberto <allanroberto@MacBook-Air-de-Allan.local>
2025-05-25 01:11:51 -0300

    Enable cors policy

--- index.js
+++ index.js
@@ -12,7 +12,14 @@ import apiRoutes from './routes/apiRoutes.js';
 const app = express();
 
 // 🔧 Middlewares
-app.use(cors({ origin: true, credentials: true }));
+
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization'],
+  credentials : true
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
